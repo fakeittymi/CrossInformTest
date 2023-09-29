06b3ca0e276232fc108f9ca44156a126b0741bf6
Master <Master@MSI>
2023-09-29 13:40:39 +0300

    Add project files.

---
+++ Tripleter/TripletSearcher.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrossinformTest.Chimpoesh.Tripleter
+{
+    /// <summary>
+    /// Поиск триплетов в тексте.
+    /// </summary>
+    public class TripletSearcher
+    {
+        /// <summary>
+        /// Длина триплета.
+        /// </summary>
+        private const int TRIPLET_LENGTH = 3;
+
+        /// <summary>
+        /// Символы-разделители слов в тексте.
+        /// </summary>
+        private readonly char[] Separators = new char[] { ' ', ',', '.', ':', '!', '?', '"', ';', '*', '(', ')', '»', '«', '…', '–', '-', '\'', '\n', '\t', '\r'};
+
+        /// <summary>
+        /// Получить частоту триплетов в тексте в один поток.
+        /// </summary>
+        /// <param name="sourceText">Исходный текст.</param>
+        /// <param name="caseSensitive">Учитывать регистр.</param>
+        /// <returns>Частоту триплетов в тексте.</returns>
+        /// <exception cref="ArgumentNullException">Возникает при передаче пустого текста.</exception>
+        public Dictionary<string, int> GetTripletsFrequencyOneThread(string sourceText, bool caseSensitive)
+        {
+            if (string.IsNullOrEmpty(sourceText))
+            {
+                throw new ArgumentNullException(nameof(sourceText), "Передана пустая строка.");
+            }
+
+            return TrimTriplets(caseSensitive
+                ? sourceText
+                : sourceText.ToLower())
+                .Where(s => s.All(ch => char.IsLetter(ch)))
+                .GroupBy(s => s)
+                .OrderByDescending(s => s.Count())
+                .ToDictionary(g => g.Key, g => g.Count());
+        }
+
+        /// <summary>
+        /// Получить частоту триплетов в тексте используя Parallel.For.
+        /// </summary>
+        /// <param name="sourceText">Исходный текст.</param>
+        /// <param name="caseSensitive">Учитывать регистр.</param>
+        /// <returns>Частоту триплетов в тексте.</returns>
+        /// <exception cref="ArgumentNullException">Возникает при передаче пустого текста.</exception>
+        public Dictionary<string, int> GetTripletsFrequencyParallelFor(string sourceText, bool caseSensitive)
+        {
+            if (string.IsNullOrEmpty(sourceText))
+            {
+                throw new ArgumentNullException(nameof(sourceText), "Передана пустая строка.");
+            }
+
+            var result = new Dictionary<string, int>();
+
+            Parallel.ForEach(Partitioner.Create(SplitIntoWords(caseSensitive
+                ? sourceText
+                : sourceText.ToLower())),
+                word =>
+                {
+                    var triplets = TrimTriplets(word);
+
+                    foreach (var triplet in triplets)
+                    {
+                        if (result.ContainsKey(triplet))
+                        {
+                            result[triplet]++;
+
+                            continue;
+                        }
+
+                        result.Add(triplet, 1);
+                    }
+                });
+
+            return result;
+        }
+
+        /// <summary>
+        /// Получить частоту триплетов в тексте используя очередь ThreadPool.
+        /// </summary>
+        /// <param name="sourceText">Исходный текст.</param>
+        /// <param name="caseSensitive">Учитывать регистр.</param>
+        /// <returns>Частоту триплетов в тексте.</returns>
+        /// <exception cref="ArgumentNullException">Возникает при передаче пустого текста.</exception>
+        public Dictionary<string, int> GetTripletsFrequencyThreadPool(string sourceText, bool caseSensitive)
+        {
+            if (string.IsNullOrEmpty(sourceText))
+            {
+                throw new ArgumentNullException(nameof(sourceText), "Передана пустая строка.");
+            }
+
+            var result = new Dictionary<string, int>();
+
+            foreach (var word in SplitIntoWords(caseSensitive
+                ? sourceText
+                : sourceText.ToLower()))
+            {
+                ThreadPool.QueueUserWorkItem(callBack =>
+                {
+                    var triplets = TrimTriplets(word);
+
+                    foreach (var triplet in triplets)
+                    {
+                        if (result.ContainsKey(triplet))
+                        {
+                            result[triplet]++;
+
+                            continue;
+                        }
+
+                        result.Add(triplet, 1);
+                    }
+                });
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Разделить текст на слова.
+        /// </summary>
+        /// <param name="sourceText">Исходный текст.</param>
+        /// <returns>Массив слов текста.</returns>
+        public IEnumerable<string> SplitIntoWords(string sourceText)
+        {
+            return sourceText.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Разделить текст на триплеты.
+        /// </summary>
+        /// <param name="source">Исходный текст.</param>
+        /// <returns>Массив триплетов текста.</returns>
+        public IEnumerable<string> TrimTriplets(string sourceText)
+        {
+            for (var position = TRIPLET_LENGTH; position <= sourceText.Length; position++)
+            {
+                yield return sourceText.Substring(position - TRIPLET_LENGTH, TRIPLET_LENGTH);
+            }
+        }
+    }
+}
---
+++ TripleterDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using CrossinformTest.Chimpoesh.Tripleter;
+
+namespace CrossinformTest.Chimpoesh.TripleterDemo
+{
+    public class Program
+    {
+        /// <summary>
+        /// Путь к файлу.
+        /// </summary>
+        private static readonly string filePath = "C:\\Users\\sserk\\OneDrive\\Desktop\\текст.txt";
+
+        static void Main(string[] args)
+        {
+            RunDemo();
+        }
+
+        /// <summary>
+        /// Запуск демонстрации.
+        /// </summary>
+        private static void RunDemo()
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(filePath);
+            }
+
+            var source = File.ReadAllText(filePath);
+            var stopwatch = new Stopwatch();
+            var tripleter = new TripletSearcher();
+
+            stopwatch.Start();
+            var triplets = tripleter
+                .GetTripletsFrequencyOneThread(source, false)
+                .OrderByDescending(v => v.Value)
+                .Take(10);
+            var count = triplets.Count();
+            stopwatch.Stop();
+            Console.WriteLine("Один поток");
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс\n");
+
+
+            stopwatch.Restart();
+            triplets = tripleter
+                .GetTripletsFrequencyParallelFor(source, false)
+                .OrderByDescending(v => v.Value)
+                .Take(10);
+            count = triplets.Count();
+            stopwatch.Stop();
+            Console.WriteLine("Parallel");
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс\n");
+
+
+            stopwatch.Restart();
+            triplets = tripleter
+                .GetTripletsFrequencyThreadPool(source, false)
+                .OrderByDescending(v => v.Value)
+                .Take(10);
+            count = triplets.Count();
+            stopwatch.Stop();
+            Console.WriteLine("ThreadPool");
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds}  мс\n");
+
+            foreach (var triplet in triplets)
+            {
+                Console.WriteLine($"{triplet.Key} - {triplet.Value}");
+            }
+        }
+    }
+}
